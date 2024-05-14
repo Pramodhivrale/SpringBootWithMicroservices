b161398200b689365e1f78c5c93a150ced6265b7
Admin <hivralepramod4@gmail.com>
2024-05-14 18:41:16 +0530

    commit

---
+++ Spring_FormDevlopment-3/src/main/java/com/data/SpringFormDevlopment3Application.java
@@ -0,0 +1,13 @@
+package com.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFormDevlopment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFormDevlopment3Application.class, args);
+	}
+
+}
